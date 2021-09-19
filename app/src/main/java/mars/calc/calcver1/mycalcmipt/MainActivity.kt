6ee3c97044c7ecb6e7993f916ce5bdd7568e745d
ScottMars <spirenkov.sm@gmail.com>
2021-09-19 10:10:29 +0300

    init commit

---
+++ app/src/main/java/mars/calc/calcver1/mycalcmipt/MainActivity.kt
@@ -0,0 +1,11 @@
+package mars.calc.calcver1.mycalcmipt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
